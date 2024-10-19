e47da1314a20285ae6b263df3c254ca0e30965cc
shubh199606 <132659915+shubh199606@users.noreply.github.com>
2024-10-19 15:44:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function isLeapYear(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return `${year} is a leap year.`;
+      } else {
+        return `${year} is not a leap year.`;
+      }
+    } else {
+      return `${year} is a leap year.`;
+    }
+  } else {
+    return `${year} is not a leap year.`;
+  }
+}
+
+console.log(isLeapYear(2024)); 
+console.log(isLeapYear(1900)); 
+console.log(isLeapYear(2000)); 
